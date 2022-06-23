a786a789907b38f3eff7c903065851355b15cf4b
albasimia <albasimia888@gmail.com>
2022-06-24 01:49:51 +0900

    音声追加。正解不正解エフェクト追加。

--- src/index.js
+++ src/index.js
@@ -10,8 +10,26 @@ import './assets/scss/main.scss'
 
 const card_data = ['watari', 'watari', 'taguchi', 'taguchi', 'ericia', 'ericia', 'rancia', 'rancia', 'chana', 'chana', 'makoto', 'makoto', 'taki', 'taki', 'makiko', 'makiko'];
 
-var watari_voice = new Howl({
-  src: ['./assets/sound/cha_watari_1.mp3']
+const voices = [];
+voices.watari = new Howl({
+  src: ['./assets/sound/watari.mp3'],
+  volume: 0.3,
+});
+voices.taguchi = new Howl({
+  src: ['./assets/sound/taguchi.mp3'],
+  volume: 1.6,
+});
+voices.rancia = new Howl({
+  src: ['./assets/sound/rancia.mp3'],
+  volume: 1.2,
+});
+const correct_sound = new Howl({
+  src: ['./assets/sound/correct.mp3'],
+  volume: 1,
+});
+const incorrect_sound = new Howl({
+  src: ['./assets/sound/incorrect.mp3'],
+  volume: 1,
 });
 
 const shuffleArray = (array) => {
@@ -30,19 +48,23 @@ const shuffleArray = (array) => {
 let game_card_data = []
 let isSelect = false
 const cards = document.querySelectorAll('.card');
+const correct = document.querySelector('.correct');
+const incorrect = document.querySelector('.incorrect');
 const init = () => {
   game_card_data = shuffleArray(card_data);
   // game_card_data = card_data;
   cards.forEach((card, key) => {
     const open_img = card.childNodes.item(1);
     open_img.setAttribute('src', `./assets/img/open.png`);
-  })
+  });
+  console.log(game_card_data);
 }
 let select_cards = null;
 const result = document.querySelector('.result');
 const openCard = (card) => {
   card.addEventListener('transitionend', () => {
-    watari_voice.play();
+    // watari_voice.play();
+    voices[game_card_data[card.dataset.cardnum]].play();
   }, {
     once: true,
   })
@@ -55,17 +77,23 @@ const openCard = (card) => {
       isSelect = true;
       setTimeout(() => {
         if (game_card_data[select_cards.dataset.cardnum] == game_card_data[card.dataset.cardnum]) {
-          result.innerText = '正解';
           card.childNodes.item(1).setAttribute('src', `./assets/img/correct.png`);
           select_cards.childNodes.item(1).setAttribute('src', `./assets/img/correct.png`);
+          correct.classList.add('active', 'animate__animated');
+          correct_sound.play();
         } else {
-          result.innerText = 'ちがうよ';
           card.classList.remove('open');
           select_cards.classList.remove('open');
+          incorrect.classList.add('active', 'animate__animated');
+          incorrect_sound.play();
         }
         select_cards = null;
-        isSelect = false;
-      }, 2000)
+        setTimeout(()=>{
+          correct.classList.remove('active', 'animate__animated');
+          incorrect.classList.remove('active', 'animate__animated');
+          isSelect = false;
+        },3000)
+      }, 3000)
     }
   }
 }
