cc87b67d1408cd18c79ee71210d2057d85a70830
albasimia <albasimia888@gmail.com>
2022-07-06 02:03:30 +0900

    画像差し替え。REPLAYボタン修正。その他レイアウト調整。

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
 // SCSS
 import './assets/scss/main.scss'
 
-const card_data = ['watari', 'watari', 'taguchi', 'taguchi', 'tanikou', 'tanikou', 'rancia', 'rancia', 'chana', 'chana', 'makoto', 'makoto', 'taki', 'taki', 'makiko', 'makiko'];
+const card_data = ['watari', 'watari', 'taguchi', 'taguchi', 'tanikou', 'tanikou', 'lancia', 'lancia', 'chana', 'chana', 'makoto', 'makoto', 'taki', 'taki', 'makiko', 'makiko'];
 
 // voice
 const voices = [];
@@ -20,7 +20,7 @@ voices.taguchi = new Howl({
   src: ['./assets/sound/taguchi.mp3'],
   volume: 1.6,
 });
-voices.rancia = new Howl({
+voices.lancia = new Howl({
   src: ['./assets/sound/rancia.mp3'],
   volume: 1.0,
 });
@@ -55,10 +55,8 @@ const incorrect_sound = new Howl({
   volume: 1,
 });
 
-let correct_count = 0;
 const shuffleArray = (array) => {
   const cloneArray = [...array]
-
   for (let i = cloneArray.length - 1; i >= 0; i--) {
     let rand = Math.floor(Math.random() * (i + 1))
     // 配列の要素の順番を入れ替える
@@ -70,28 +68,33 @@ const shuffleArray = (array) => {
 }
 
 let game_card_data = []
+let correct_count = 0;
 let is_select = false
+let is_all_clear = false;
+let select_cards = null;
+let is_correct = false;
 const cards = document.querySelectorAll('.card');
 const correct = document.querySelector('.correct');
 const incorrect = document.querySelector('.incorrect');
+const result = document.querySelector('.result');
 const init = () => {
+  correct_count = 0;
+  is_select = false
+  is_all_clear = false;
+  select_cards = null;
+  is_correct = false;
   game_card_data = shuffleArray(card_data);
   // game_card_data = card_data;
   cards.forEach((card, key) => {
     card.classList.remove('open');
+    card.childNodes.item(0).setAttribute('src', `./assets/img/close_cha.png`)
     const open_img = card.childNodes.item(1);
     open_img.setAttribute('src', `./assets/img/mesumaru/${key}.png`);
   });
-  correct_count = 0;
-  console.log(game_card_data); // あとで消す。
 }
-let select_cards = null;
-let is_correct = false;
-const result = document.querySelector('.result');
 const openCard = (card) => {
-  if (!is_select) {
+  if (!is_select && !is_all_clear) {
     card.addEventListener('transitionend', () => {
-      // watari_voice.play();
       voices[game_card_data[card.dataset.cardnum]].play();
     }, {
       once: true,
@@ -121,14 +124,13 @@ const openCard = (card) => {
           incorrect.classList.remove('active', 'animate__animated');
           if (is_correct) {
             correct_count++;
-            console.log(correct_count)
             if (correct_count == 8) {
               allClear();
             }
           }
           is_select = false;
           is_correct = false;
-        }, 3000)
+        }, 2500)
       }, 3000)
     }
   }
@@ -138,10 +140,10 @@ const allClear = () => {
   cards.forEach(function (card, key) {
     card.childNodes.item(0).setAttribute('src', `./assets/img/mesumaru/${key}.png`);
     card.classList.remove('open')
-    console.log('all clear');
-    const all_clear_ele = document.querySelector('.allClear');
-    all_clear_ele.classList.add('clear');
   })
+  const all_clear_ele = document.querySelector('.allClear');
+  all_clear_ele.classList.add('clear');
+  is_all_clear = true;
 }
 
 cards.forEach(function (card) {
